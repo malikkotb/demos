84933a97ca117020edfc015d3699f4a2b385f8fe
Malik Kotb <malikkotb@icloud.com>
2024-01-15 10:00:34 +0200

    layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@
 export default function Home() {
   return (
    <main>
-    <div>navbar with list of projects</div>
+    <nav className="flex text-white items-center p-4 justify-between h-[5vw] bg-slate-600 w-screen top-0 sticky">
+      <div>Malik ↗</div>
+      <div>Popdown</div>
+      <div>hi</div>
+    </nav>
     <div>Aniamted COunter</div>
    </main>
   )

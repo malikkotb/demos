63886eae31128cd9b9d67d3204d3ac278c66bf53
Malik Kotb <malikkotb@icloud.com>
2024-02-04 17:00:28 +0100

    paths changed

--- app/horizontal-section/components/HorizontalScroll.tsx
+++ app/horizontal-section/components/HorizontalScroll.tsx
@@ -66,7 +66,7 @@ export default function HorizontalScroll() {
             </div>
             <div className="relative h-[full] w-[50%] overflow-hidden">
               <Image
-                src={"./16.jpg"} 
+                src={"./16.JPG"} 
                 alt="Description"
                 fill
               />
